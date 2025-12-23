fbae4a278fc4224f8673a30b0ce76d6d9ce71b76
praveen05pk <77532525+praveen05pk@users.noreply.github.com>
2025-12-23 21:45:39 +0530

    Fix formatting and indentation in main method

--- backend/src/main/java/com/smarttech/SmartTechApplication.java
+++ backend/src/main/java/com/smarttech/SmartTechApplication.java
@@ -1,6 +1,12 @@
+package com.smarttech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class SmartTechApplication {
- public static void main(String[] args) {
-  SpringApplication.run(SmartTechApplication.class, args);
- }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartTechApplication.class, args);
+    }
 }
